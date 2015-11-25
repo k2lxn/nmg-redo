63f5b0d165224cab0d5eeb7cc0b464d16bfea00e
Jane <jane.e.reynolds@gmail.com>
2015-11-24 21:45:30 -0800

    Add ability to update mortgage calculator

--- app/controllers/mortgage_calculators_controller.rb
+++ app/controllers/mortgage_calculators_controller.rb
@@ -7,5 +7,17 @@ class MortgageCalculatorsController < ApplicationController
   end
 
   def update
+  	@mrtg_calc = MortgageCalculator.find(1)
+  	
+  	@mrtg_calc.update(calc_params)
+  	
+  	render "edit"
   end
+  
+  private
+  	
+  	def calc_params
+  		params.require(:mortgage_calculator).permit(:loan_amt, :rate, :term)
+  	end
+  
 end
--- app/helpers/mortgage_calculators_helper.rb
+++ app/helpers/mortgage_calculators_helper.rb
@@ -1,2 +1,9 @@
 module MortgageCalculatorsHelper
+	# Takes an InputField
+	def select_options(field_name)
+  	select_options = {"term" => [["15yr", 15], ["30yr", 30]], 
+							"rate" => (3.0..5.5).step(0.125),
+							"down" => (10.0..30.0).step(0.25)}
+  	select_options[field_name]
+  end
 end
