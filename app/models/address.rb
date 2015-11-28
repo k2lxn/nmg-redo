9784338442a6315ca0c160742c9af7728a807335
Jane <jane.e.reynolds@gmail.com>
2015-11-28 15:33:07 -0800

    Write basic tests for Address model (without address verification)

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,8 @@
 class Address < ActiveRecord::Base
 	validates :street_1, presence: true
 	validates :city, presence: true
-	validates :state, presence: true
-	validates :zip_code, presence: true
+	validates :state, presence: true, length: { maximum: 2 }
+	validates :zip_code, presence: true, numericality: true
 	
 	belongs_to :borrower
 end
--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
-class AddressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class AddressTest < ActiveSupport::TestCase	
+	def setup												
+  	@address = Address.new( street_1: "123 Fake St",city: "Washington D.C.",state: "MD",
+  													zip_code: "20004")
+  end
+  
+  test "should be valid" do
+  	assert @address.valid?
+  end
+  
+  test "street should be present" do
+		@address.street_1 = "      "
+		assert_not @address.valid?
+	end
+  
+  test "city should be present" do
+  	@address.city = "      "
+  	assert_not @address.valid?
+  end
+  
+  test "state should be present" do
+  	@address.state = "      "
+  	assert_not @address.valid?
+  end
+  
+  test "zip code should be present" do
+  	@address.zip_code = "      "
+  	assert_not @address.valid?
+  end	
+  
+  test "state should be two characters" do
+  	@address.state = "CAA"
+  	assert_not @address.valid?
+  end
+  
+  test "zip code should be numerical" do
+  	@address.zip_code = "A94151"
+  	assert_not @address.valid?
+  end
 end
