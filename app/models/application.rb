12d62f516b56e2e22919e1ba1642e2196abcced5
Jane <jane.e.reynolds@gmail.com>
2015-11-28 16:08:37 -0800

    Add Application model tests and validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,7 @@
 class Application < ActiveRecord::Base
+	validates :loan_purpose, :property_type, presence: true
+	validates :purchase_price, :budget, presence: true, numericality: true
+	validates :zip_code, presence: true
+	
 	belongs_to :borrower
 end
--- test/models/application_test.rb
+++ test/models/application_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class ApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+  	@application = Application.new( loan_purpose: "purchase",property_type: "primary",
+  																purchase_price: 500000,budget: 20000,zip_code: "94110")  	
+  end
+  
+  test "should be valid" do 
+  	assert @application.valid?
+  end
+  
+  test "loan purpose should be present" do
+  	@application.loan_purpose = "     "
+  	assert_not @application.valid?
+  end
+  
+  test "property type should be present" do
+  	@application.property_type = "     "
+  	assert_not @application.valid?
+  end
+  
+  test "purchase price should be present" do
+  	@application.purchase_price = "     "
+  	assert_not @application.valid?
+  end
+  
+  test "budget should be present" do
+  	@application.budget = "     "
+  	assert_not @application.valid?
+  end
+  
+  test "zip code should be present" do
+  	@application.zip_code = "     "
+  	assert_not @application.valid?
+  end
+  
+  test "purchase price should be numerical" do
+  	@application.purchase_price = "$14,000"
+  	assert_not @application.valid?
+  end
+  
+  test "budget should be numerical" do
+  	@application.budget = "140.00.000"
+  	assert_not @application.valid?
+  end
 end
