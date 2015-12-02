0c6dda5ab1cb25ee44e3958dd3fd62adde253af1
Jane <jane.e.reynolds@gmail.com>
2015-12-02 13:28:03 -0800

    Fix bug: only displaying first digit of state abrreviations

--- app/controllers/borrowers_controller.rb
+++ app/controllers/borrowers_controller.rb
@@ -58,10 +58,11 @@ class BorrowersController < ApplicationController
 		end
 	
 		def state_abbreviations
-			["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA",
+			abbrevs = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA",
 				"KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ",
 				"NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT",
 				"VA","WA","WV","WI","WY" ]
+			abbrevs.map { |abr| [abr, abr] }	
 		end
   
 end
