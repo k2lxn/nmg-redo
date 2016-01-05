4050976bc6cfdf9be31e73078748c26766c3c41a
Jane <jane.e.reynolds@gmail.com>
2016-01-05 10:54:27 -0800

    Delete obsolete routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
 	
 	get '/apply', to: "borrowers#new"
 	
-	#post '/apply', to: "borrowers#create", as: "confirm"
-	
-	#get '/confirm', to: "static_pages#confirm" 
-	
 	post '/confirm', to: "borrowers#create"
 	
 	get '/tools', to: "static_pages#tools"
