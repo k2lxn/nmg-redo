10f6ca57dc212398856a8871c2b5e3f7e78595ce
Jane <jane.e.reynolds@gmail.com>
2016-01-05 18:56:30 -0800

    Display model data in worksheet

--- app/models/borrower.rb
+++ app/models/borrower.rb
@@ -15,20 +15,23 @@ class Borrower < ActiveRecord::Base
 	has_one :coborrower
 	
 	accepts_nested_attributes_for :application, :address, :coborrower
-	
+
 	def save_application
 		# Create session. Prompts credential via command line the first time, 
 		# then saves to given file for later usage
 		session = GoogleDrive.saved_session("config.json")
 		
 		# Get worksheet from spreadsheet
-		ws = session.spreadsheet_by_key("1Ntt_7-UBorOXXx6_PFfK_cE9_ElTXcQZfaebMQgBNmA").worksheets[0]
+		loan_ws = session.spreadsheet_by_key("1Ntt_7-UBorOXXx6_PFfK_cE9_ElTXcQZfaebMQgBNmA").worksheets[0]
+		contact_ws = session.spreadsheet_by_key("1Ntt_7-UBorOXXx6_PFfK_cE9_ElTXcQZfaebMQgBNmA").worksheets[1]
 		
-		next_row = ws.num_rows + 1
+		next_row = loan_ws.num_rows + 1
 		
-		ws[next_row, 1] = "hi jane!"
-		ws.save
-		ws.reload
+		# Write loan information (i.e. Application data) 
+		loan_ws[next_row, 1] = self.first_name
+		loan_ws.save
+		
+		loan_ws.reload
 	end
 	
 end
