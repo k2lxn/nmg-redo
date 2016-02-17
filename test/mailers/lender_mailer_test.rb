a93f33875b1bb64ccd5e002330eb5317da9e0341
Jane <jane.e.reynolds@gmail.com>
2016-02-17 11:58:33 -0800

    Create stub Lender Mailer

---
+++ test/mailers/lender_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LenderMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/lender_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/lender_mailer
+class LenderMailerPreview < ActionMailer::Preview
+
+end
