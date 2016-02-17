f2fa8b26645e79aac90d12483bf00192764dc225
Jane <jane.e.reynolds@gmail.com>
2016-02-17 14:26:48 -0800

    Configure smtp for SendGrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+	:user_name => ENV["SEND_GRID_USERNAME"],
+	:password => ENV["SEND_GRID_PASSWORD"],
+	:domain => 'heroku.com',
+	:address => 'smtp.sendgrid.net',
+	:port => 587,
+	:authentication => :plain,
+	:enable_starttls_auto => true
+}
