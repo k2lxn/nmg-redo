78be227c24c855a5eb728756755cec346c324276
Jane <jane.e.reynolds@gmail.com>
2016-02-17 12:21:27 -0800

    Create stub views and preview for LenderMailer#lead_alert

--- test/mailers/previews/lender_mailer_preview.rb
+++ test/mailers/previews/lender_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/lender_mailer
 class LenderMailerPreview < ActionMailer::Preview
-
+	def lead_alert_preview
+		LenderMailer.lead_alert(Borrower.last)
+	end
+	
 end
