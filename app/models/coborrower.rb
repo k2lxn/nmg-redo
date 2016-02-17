4008aea6837262fd51bffebd8be4191f8bcddb2f
Jane <jane.e.reynolds@gmail.com>
2016-02-17 09:20:17 -0800

    Comment out data validations for testing purposes (dev only)

--- app/models/coborrower.rb
+++ app/models/coborrower.rb
@@ -1,7 +1,7 @@
 class Coborrower < ActiveRecord::Base
-	validates :spouse, presence: true
-	validates :first_name, presence: true, length: { maximum: 35 } 
-	validates :last_name, presence: true, length: { maximum: 45 }
+	#validates :spouse, presence: true
+	#validates :first_name, presence: true, length: { maximum: 35 } 
+	#validates :last_name, presence: true, length: { maximum: 45 }
 	
 	belongs_to :borrower
 end
