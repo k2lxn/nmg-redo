7e48abd043760fbcc67bac045a364fbb78597a0b
Jane <jane.e.reynolds@gmail.com>
2016-02-17 18:41:25 -0800

    Update LenderMailer :from address and subject line

--- app/mailers/lender_mailer.rb
+++ app/mailers/lender_mailer.rb
@@ -1,9 +1,9 @@
 class LenderMailer < ApplicationMailer
-	default from: "jane.e.reynolds@gmail.com", to: "jane.e.reynolds@gmail.com"
+	default from: "alert@nationwidemtg.us", to: "jane.e.reynolds@gmail.com"
 	
 	def lead_alert(borrower)
 		@borrower = borrower
-		mail(subject: "Test 02/17")
+		mail(subject: "New loan applicant")
 	end
 	
 end
