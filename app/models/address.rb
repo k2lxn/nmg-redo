5164bd9877f8415e3bd2aa58186ad2fa6f81adff
Jane <jane.e.reynolds@gmail.com>
2016-02-19 15:50:55 -0800

    Remove redundant zip_code length validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,7 @@ class Address < ActiveRecord::Base
 	validates :street_1, presence: true
 	validates :city, presence: true
 	validates :state, presence: true, length: { maximum: 2 }
-	validates :zip_code, presence: true, length: { is: 5 }, format: { with: /\A\d{5}\z/ }
+	validates :zip_code, presence: true, format: { with: /\A\d{5}\z/ }
 	
 	belongs_to :borrower
 end
