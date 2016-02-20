c99e744c9ceeabe5f2f586047f60ddb823188a18
Jane <jane.e.reynolds@gmail.com>
2016-02-19 16:22:34 -0800

    Display only custom error messages to suppress field names

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,8 @@ class Address < ActiveRecord::Base
 	validates :street_1, presence: true
 	validates :city, presence: true
 	validates :state, presence: true, length: { maximum: 2 }
-	validates :zip_code, presence: true, format: { with: /\A\d{5}\z/ }
+	validates :zip_code, presence: true, 
+											 format: { with: /\A\d{5}\z/, message: "Please enter a 5 digit Zip Code" }
 	
 	belongs_to :borrower
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -4,7 +4,8 @@ class Application < ActiveRecord::Base
 																			numericality: true
 																			#:format => { with: /\d{1,3}(\,\d{3})*\.\d{2}/ }
 																			#:format => { with: /\A\d{1,4}(\.\d{0,2})?\z/ }
-	validates :zip_code, presence: true, format: { with: /\A\d{5}\z/ }
+	validates :zip_code, presence: true, format: { with: /\A\d{5}\z/, message: "Please enter a 5 digit Zip Code" }
+											 
 	
 	belongs_to :borrower
 end
